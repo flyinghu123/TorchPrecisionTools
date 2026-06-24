9997584333fd16f414a126c9785570fee9f43854
flyinghu123 <1152598046@qq.com>
2026-06-24 16:40:52 +0800

    实现hook自动注入

--- tpt/__init__.py
+++ tpt/__init__.py
@@ -1,7 +1,3 @@
 # -*- coding: utf-8 -*-
 
 __version__ = "0.1.0"
-
-
-def hello_world():
-    print("Hello from torch-precision-tools! This package was automatically loaded.")
--- tpt/auto.py
+++ tpt/auto.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 # This module is auto-imported via torch_precision_tools.pth on Python startup.
 
-from tpt import hello_world
+import os
 
-hello_world()
+
+if str(os.getenv('ENABLE_TPT', None)).lower() in ['true', '1', 'on']:
+    print('ENABLE TPT AUTO HOOK')
+    import torch.nn as nn
+    from tpt.base import BaseModule
+    nn.Module = BaseModule
